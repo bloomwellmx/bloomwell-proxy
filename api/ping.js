f37311a56db457f35efac7efbc081ec8caad1713
bloomwellmx <hola@bloomwell.mx>
2026-01-17 16:50:51 -0600

    add ping endpoint

---
+++ api/ping.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  return res.status(200).json({
+    ok: true,
+    message: "ping works",
+    timestamp: Date.now()
+  });
+}
