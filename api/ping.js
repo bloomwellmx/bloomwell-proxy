8ea49cebaabff7be4108d6cc7b77ffccf3fd5726
bloomwellmx <hola@bloomwell.mx>
2026-01-17 16:55:10 -0600

    add cors to ping

--- api/ping.js
+++ api/ping.js
@@ -1,4 +1,13 @@
 export default function handler(req, res) {
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   return res.status(200).json({
     ok: true,
